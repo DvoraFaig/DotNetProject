6b1bf584ac1cbcd0e8596ea44f0973ebbec9d988
ShiffyDishonSchool <handesaim3A8@mbjcomp.org.il>
2021-10-13 19:03:05 +0300

    changes

--- dotNet5782_4228_1070/DAL/Enum.cs
+++ dotNet5782_4228_1070/DAL/Enum.cs
@@ -5,10 +5,6 @@ namespace IDAL
 {
     namespace DO
     {
-        public class Enum
-        {
-
-        }
         enum WeightCategories { Light, Medium, Heavy }
         enum DroneStatus { Available, Maintenance, Delivery }
         enum Priorities { Regular, Fast, Emergency }
