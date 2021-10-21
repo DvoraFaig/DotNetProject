82eef7ec7cbc5c15b96982ac237d3ccc4155f00c
Dvora Faig <dvora.faig@gmail.com>
2021-10-21 20:56:11 +0300

    -

--- dotNet5782_4228_1070/DAL/Customer.cs
+++ dotNet5782_4228_1070/DAL/Customer.cs
@@ -11,6 +11,7 @@ namespace DalObject
         public int ID { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
+
         public double Longitude { get; set; }
         public double Latitude { get; set; }
         public override string ToString()
