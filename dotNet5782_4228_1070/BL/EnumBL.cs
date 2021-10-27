f781266ee3e38367f5e64a607383e3cd6e2ddffc
ShiffyDishonSchool <handesaim3A8@mbjcomp.org.il>
2021-10-27 13:07:54 +0300

    Creat new project BL and EnumBL

---
+++ dotNet5782_4228_1070/BL/EnumBL.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace IBL.BO
+{
+    public enum WeightCategories { Light, Medium, Heavy }
+    public enum DroneStatus { Available, Maintenance, Delivery }
+    public enum Priorities { Regular, Fast, Emergency }
+}
