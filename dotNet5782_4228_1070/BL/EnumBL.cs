f3424d8479a075a77cafc269e572efa8a05e78d1
ShiffyDishonSchool <handesaim3A8@mbjcomp.org.il>
2021-10-27 14:40:35 +0300

    export the enums (of switch)  in the mainBL to EnumBl

--- dotNet5782_4228_1070/BL/EnumBL.cs
+++ dotNet5782_4228_1070/BL/EnumBL.cs
@@ -5,4 +5,10 @@ namespace IBL.BO
     public enum WeightCategories { Light, Medium, Heavy }
     public enum DroneStatus { Available, Maintenance, Delivery }
     public enum Priorities { Regular, Fast, Emergency }
+
+    //enum for the main
+    enum Choices { Add = 1, Update, ShowWithId, ShowList, exit }
+    enum objects { Station = 1, Drone, Customer, Parcel, FreeParcel, EmptyCharges }
+    enum UpdateObj { DroneReceivesParcel = 1, DroneCollectsAParcel, CostumerGetsParcel, sendDroneToCharge, freeDroneFromCharge }
+
 }
