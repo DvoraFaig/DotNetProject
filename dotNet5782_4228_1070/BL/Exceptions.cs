19ae014fbe31889261bc97835b674fcd532a9be1
ShiffyDishonSchool <handesaim3A8@mbjcomp.org.il>
2021-11-06 20:07:54 +0200

    Exeptions ibn BL : InvalidStringException , ObjNotExistException

--- dotNet5782_4228_1070/BL/Exceptions.cs
+++ dotNet5782_4228_1070/BL/Exceptions.cs
@@ -8,7 +8,23 @@ namespace IBL
     {
         class Exceptions
         {
-            // like his: 
+            class InvalidStringException : Exception
+            {
+                public InvalidStringException() { }
+                public InvalidStringException(string name)
+                    : base(String.Format("Invalid string : {0}", name))
+                {
+                }
+            }
+            class ObjNotExistException<T> : Exception
+            {
+                public ObjNotExistException(){ }
+                public ObjNotExistException( T obj )
+                    : base(String.Format($"The {obj.GetType()} doesn't exist."))
+                {
+                }
+            }
+
         }
     }
 }
