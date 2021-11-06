534e15bc65fcebd28ffdcde2e7f563f1856238ea
ShiffyDishonSchool <handesaim3A8@mbjcomp.org.il>
2021-11-06 23:33:12 +0200

    BLdataSource added a class to all the lists of objects

--- dotNet5782_4228_1070/BL/BL/BLdataSourc.cs
+++ dotNet5782_4228_1070/BL/BL/BLdataSourc.cs
@@ -11,10 +11,13 @@ namespace BL
 {
     public partial class BL //: IBL
     {
-        internal static List<IBL.BO.Drone> Drones = new List<IBL.BO.Drone>();
-        internal static List<IBL.BO.Station> Stations= new List<IBL.BO.Station>();
-        internal static List<IBL.BO.Customer> Customers = new List<IBL.BO.Customer>();
-        internal static List<IBL.BO.Parcel> Parcels  = new List<IBL.BO.Parcel>();
-        internal static List<IBL.BO.DroneCharge> DroneCharges  = new List<IBL.BO.DroneCharge>();
+        public class BLdataSource
+        {
+            internal static List<IBL.BO.Drone> Drones = new List<IBL.BO.Drone>();
+            internal static List<IBL.BO.Station> Stations = new List<IBL.BO.Station>();
+            internal static List<IBL.BO.Customer> Customers = new List<IBL.BO.Customer>();
+            internal static List<IBL.BO.Parcel> Parcels = new List<IBL.BO.Parcel>();
+            internal static List<IBL.BO.DroneCharge> DroneCharges = new List<IBL.BO.DroneCharge>();
+        }
     }
 }
