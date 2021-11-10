98441a24436179781bc6bce3ef683114dce218c5
ShiffyDishonSchool <handesaim3A8@mbjcomp.org.il>
2021-11-10 14:58:19 +0200

    BL ctor

--- dotNet5782_4228_1070/BL/BL/BL.cs
+++ dotNet5782_4228_1070/BL/BL/BL.cs
@@ -20,6 +20,47 @@ namespace BL
         {
             dronesInBL = new List<BLDrone>();
             dal = IDal.DalFactory.factory("DalObject");
+            Random r = new Random();
+            List<IDal.DO.Drone> drones = dal.displayDrone().Cast<IDal.DO.Drone>().ToList();
+            List<IDal.DO.Parcel> parcels = dal.displayDrone().Cast<IDal.DO.Parcel>().ToList();
+            BLDrone BLd;
+            drones.ForEach(d =>
+            {
+                BLd = convertDalToBLDrone(d);
+
+                parcels.ForEach(p =>
+                {
+                    if (!p.Scheduled.Equals(null) && p.Delivered.Equals(null))// pair a parcel to drone but not yed delivered.
+                    {
+                        IDal.DO.Customer c;
+                        if (p.PickUp.Equals(null)) //if parcel wasn't picked up
+                        {
+                            c = dal.getCustomerById(p.SenderId);
+                        }
+                        else //if parcel was picked up but wasn't delivered
+                        {
+                            c = dal.getCustomerById(p.TargetId);
+                        }
+                        BLd.DronePosition = new BLPosition() { Longitude = c.Longitude, Latitude = c.Latitude };
+                        BLd.Battery = r.Next(20, 100); // מצב סוללה יוגרל בין טעינה מינימלית שתאפשר לרחפן לבצע את המשלוח ולהגיע לטעינה לתחנה הקרובה ליעד המשלוח לבין טעינה מלאה
+                    }
+                });
+                if((parcels.Find(p => p.DroneId == d.Id)).Equals(null)) //if the drone is not requested to a parcel - 
+                {
+                    BLd.Status = (DroneStatus)r.Next(0, 2);
+                }
+                //if(d.Status == DroneStatus.Maintenance) // doesn't have this by the dal
+                //{
+                //     //להגריל מספ תחנת בסיס 
+                //    d.Battery = r.Next(0,20)
+                //}
+                //if(d.Status == DroneStatus.Availble) // doesn't have this by the dal
+                //{
+                //    
+                //}
+            });
+            //add drones to BLDrone arr
+            
             /*
             //יש לבקש משכבת הנתונים ולשמור בשדות נפרדים את צריכת החשמל ע"י
             //הרחפנים ואת קצב טעינתם -בהתאם למה שרשום לעיל
--- dotNet5782_4228_1070/BL/IBL.BO/objects/BLobjects.cs
+++ dotNet5782_4228_1070/BL/IBL.BO/objects/BLobjects.cs
@@ -26,7 +26,7 @@ namespace IBL
             public int Id { get; set; }
             public WeightCategories Weight { get; set; }
             public Priorities Priority { get; set; }
-            public bool parcelStatus { get; set; }
+            public bool parcelStatus { get; set; } //ממתין לאיסוף =false\ בדרך ליעד=true
             public BLPosition SenderPosition { get; set; }
             public BLPosition TargetPosition { get; set; }
             public double distance { get; set; } //.............. } //sqrt(pow(SenderPosition.Latitude+TargetPosition.Latitude,2)+ pow(SenderPosition.Longitude+TargetPosition.Longitude,2),2)}
