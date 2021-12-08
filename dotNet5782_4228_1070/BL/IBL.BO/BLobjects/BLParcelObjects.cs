884563decc20ae6399332dbbdda1eb33c0212670
Dvori Faig <dvora.faig@gmail.com>
2021-12-08 17:44:00 +0200

    ם

--- dotNet5782_4228_1070/BL/IBL.BO/BLobjects/BLParcelObjects.cs
+++ dotNet5782_4228_1070/BL/IBL.BO/BLobjects/BLParcelObjects.cs
@@ -24,7 +24,7 @@ namespace IBL
             public DateTime? Delivered { get; set; }
             public override string ToString()
             {
-                string notFilled = "not filled";
+                //string notFilled = "not filled";
                 return ($"parcel ID: {Id}, \n\tSender: {Sender.ToString()}, \tTarget: {Target.ToString()}, \tparcel Priority: {Priority}, parcel weight: {Weight},\n" +
                     $"\n\tDrone: {(!Drone.Equals(default(BLDroneInParcel))? Drone.ToString() : (char)'-') } " +
                     $"\n\tparcel Requeasted: {(Requeasted != null? Requeasted : (char)'-' )}, parcel scheduled: {(Scheduled != null ? Scheduled :  (char)'-' )}, parcel pickUp: {(PickUp != null ? PickUp : (char)'-')}, parcel delivered: {(Delivered != null ? Delivered : (char)'-')}\n");
