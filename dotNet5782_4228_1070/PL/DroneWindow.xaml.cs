21fe6bfc3dff51e07c7d77b2f0d6fab5e96878ff
ShiffyDishonSchool <handesaim3A8@mbjcomp.org.il>
2021-12-09 01:00:45 +0200

    DroneWindow Add add id,model,wight....

--- dotNet5782_4228_1070/PL/DroneWindow.xaml.cs
+++ dotNet5782_4228_1070/PL/DroneWindow.xaml.cs
@@ -27,14 +27,23 @@ namespace PL
         {
             InitializeComponent();
             UpdateDroneDisplay.Visibility = Visibility.Hidden;
+            WeightSelector.ItemsSource = Enum.GetValues(typeof(IDal.DO.WeightCategories));
+            IdTextBox.Text = "Drone id....";
+            ModelTextBox.Text = "Model id....";
             blObjectD = blObject;
 
         }
+
         private void OnClosing1(object sender, CancelEventArgs e)
         {
            e.Cancel = true;
         }
-
+        // TextChangedEventHandler delegate method.
+        private void textChangedEventHandler(object sender, TextChangedEventArgs args)
+        {
+            // Omitted Code: Insert code that does something whenever
+            // the text changes...
+        } // end textChangedEventHandler
         public DroneWindow(IBL.Ibl blObject, IBL.BO.BLDrone drone)
         {
             InitializeComponent();
@@ -48,8 +57,13 @@ namespace PL
             this.Close();
         }
 
-        private void onClosing1(object sender, EventArgs e)
+        private void WeightSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+
         }
+
+        //private void onClosing1(object sender, EventArgs e)
+        //{
+        //}
     }
 }
