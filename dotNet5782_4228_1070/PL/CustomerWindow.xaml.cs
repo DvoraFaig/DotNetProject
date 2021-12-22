c0c1efbaefdecd54836bd1c6d30fe1985a31e350
Shiffy Dishon <handesaim3A8@mbjcomp.org.il>
2021-12-22 14:02:28 +0200

    CustomerWindow

--- dotNet5782_4228_1070/PL/CustomerWindow.xaml.cs
+++ dotNet5782_4228_1070/PL/CustomerWindow.xaml.cs
@@ -46,7 +46,6 @@ namespace PL
             PhoneTextBox.Text = "Phone...";
             LatitudeTextBox.Text = "latitude...";
             LongitudeTextBox.Text = "longitude...";
-            PositionTextBox.Text = $"( {customer.CustomerPosition.Latitude} , {customer.CustomerPosition.Longitude} )";
             visibleAddForm.Visibility = Visibility.Visible;
             visibleUpdateForm.Visibility = Visibility.Hidden;
         }
@@ -65,6 +64,8 @@ namespace PL
             PositionTextBox.Text = $"( {customer.CustomerPosition.Latitude} , {customer.CustomerPosition.Longitude} )";
             CustomerAsTargetListView.ItemsSource = customerInCtor.CustomerAsSender;
             CustomerAsSenderListView.ItemsSource = customerInCtor.CustomerAsTarget;
+            CustomerAsTargetListView.Visibility = Visibility.Hidden;
+
         }
 
         private void setDeliveryButton()
@@ -209,6 +210,11 @@ namespace PL
             //    catch (BO.Exceptions.ObjNotExistException e1) { MessageBox.Show(e1.Message); }
             //    catch (Exception e2) { MessageBox.Show(e2.Message); }
             //}
+        }
+
+        private void show(object sender, MouseButtonEventArgs e)
+        {
+            CustomerAsTargetListView.Visibility = Visibility.Visible;
         }
     }
 }
